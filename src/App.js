184fa06a8cde15e5e32dd7eed8938322943d6b33
Precious <Preshomondi@gmail.com>
2022-11-28 21:25:46 +0300

    create new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
-import './App.css';
+
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import List from './components/List/List';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+import {CssBaseline, Grid} from "@material-ui/core"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <CssBaseline/>
+    <Header/>
+    <Grid container spacing={3} style={{width: '100%'}}>
+      <Grid item xs={12} md={4}>
+        <List/>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Map/>
+      </Grid>
+    </Grid>
+    </>
   );
 }
 
